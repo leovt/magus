fa2abbcfc26933e4e361eba9f59ccb321123fd82
leovt <leonhard.vogt@gmx.ch>
2018-11-14 21:09:12 +0100

    improved dragging of icons

--- client.js
+++ client.js
@@ -1,12 +1,23 @@
 var selection;
-var start_drag_event;
+var mousepos_off_x;
+var mousepos_off_y;
+
+function cursorPoint(evt){
+  var svg = document.querySelector('svg');
+  var pt = svg.createSVGPoint();
+  pt.x = evt.clientX; pt.y = evt.clientY;
+  return pt.matrixTransform(svg.getScreenCTM().inverse());
+}
 
 function startDrag(evt){
+  if (evt.button != 0) {return;}
   console.log(evt.target);
   console.log(evt.target.parentElement);
   if (evt.target.parentElement.classList.contains('icon')) {
     selection = evt.target.parentElement;
-    start_drag_event = evt;
+    var pt = cursorPoint(evt);
+    mousepos_off_x = pt.x - selection.transform.baseVal.getItem(0).matrix.e;
+    mousepos_off_y = pt.y - selection.transform.baseVal.getItem(0).matrix.f;
   } else {
     selection = null;
   }
@@ -14,7 +25,8 @@ function startDrag(evt){
 
 function moveDrag(evt){
   if (selection) {
-    selection.transform.baseVal.getItem(0).setTranslate(evt.clientX, evt.clientY);
+    var pt = cursorPoint(evt);
+    selection.transform.baseVal.getItem(0).setTranslate(pt.x-mousepos_off_x, pt.y-mousepos_off_y);
   }
 }
 
