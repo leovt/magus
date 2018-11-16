caa7c9a38af0fd3065465f90a34f1b1a03a46550
leovt <leonhard.vogt@gmx.ch>
2018-11-16 23:05:01 +0100

    modularizing javascript by moving all drag related stuff in an enclosing function setup_drag

--- client.js
+++ client.js
@@ -1,33 +1,41 @@
-var selection;
-var mousepos_off_x;
-var mousepos_off_y;
-
-function cursorPoint(evt){
+function setup_dragdrop(){
+  var selection;
+  var mousepos_off_x;
+  var mousepos_off_y;
   var svg = document.querySelector('svg');
-  var pt = svg.createSVGPoint();
-  pt.x = evt.clientX; pt.y = evt.clientY;
-  return pt.matrixTransform(svg.getScreenCTM().inverse());
-}
 
-function startDrag(evt){
-  if (evt.button != 0) {return;}
-  if (evt.target.parentElement.classList.contains('icon')) {
-    selection = evt.target.parentElement;
-    var pt = cursorPoint(evt);
-    mousepos_off_x = pt.x - selection.transform.baseVal.getItem(0).matrix.e;
-    mousepos_off_y = pt.y - selection.transform.baseVal.getItem(0).matrix.f;
-  } else {
+  function cursorPoint(evt){
+    var pt = svg.createSVGPoint();
+    pt.x = evt.clientX; pt.y = evt.clientY;
+    return pt.matrixTransform(svg.getScreenCTM().inverse());
+  }
+
+  function startDrag(evt){
+    if (evt.button != 0) {return;}
+    if (evt.target.parentElement.classList.contains('icon')) {
+      selection = evt.target.parentElement;
+      var pt = cursorPoint(evt);
+      mousepos_off_x = pt.x - selection.transform.baseVal.getItem(0).matrix.e;
+      mousepos_off_y = pt.y - selection.transform.baseVal.getItem(0).matrix.f;
+    } else {
+      selection = null;
+    }
+  }
+
+  function moveDrag(evt){
+    if (selection) {
+      var pt = cursorPoint(evt);
+      selection.transform.baseVal.getItem(0).setTranslate(pt.x-mousepos_off_x, pt.y-mousepos_off_y);
+    }
+  }
+
+  function endDrag(evt){
     selection = null;
   }
-}
 
-function moveDrag(evt){
-  if (selection) {
-    var pt = cursorPoint(evt);
-    selection.transform.baseVal.getItem(0).setTranslate(pt.x-mousepos_off_x, pt.y-mousepos_off_y);
-  }
-}
+  svg.addEventListener('mousedown', startDrag);
+  svg.addEventListener('mousemove', moveDrag);
+  svg.addEventListener('mouseup', endDrag);
+};
 
-function endDrag(evt){
-  selection = null;
-}
+document.addEventListener("DOMContentLoaded", setup_dragdrop);
